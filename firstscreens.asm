b45abfcd19a0d9a9f1387acc7c3ca92aa5c3e1fc
nepecute <nepecute@penguin.lxd>
2021-06-08 18:27:35 +0200

    added the text "press start" on the title screen, as a placeholder to both test my VBlank renderer and have a rudimentary title screen ready

--- firstscreens.asm
+++ firstscreens.asm
@@ -1,4 +1,4 @@
-  ld a, [isFirstFrame]
+  ld a, [isFirstFrame] ;checks if it's the first frame of that specific game state
   or a, 0
   jr z, .skipFirstScreenLoading
 
@@ -26,6 +26,7 @@
   ld [wVRAMCopyLen], a
 
   xor a, a
+  ld [wVRAMCopyType], a
   ld c, 13
   ld hl, wVRAMCopyBuffer
 .zerofillBuffer
@@ -46,9 +47,33 @@
 
   halt
 
+  ld a, PRESS_START_SIZE
+  ld [wVRAMCopyLen], a
+  ld c, a
+
+  ld a, $9A
+  ld [wVRAMCopyDest], a
+  ld a, $04
+  ld [wVRAMCopyDest+1], a
+
+  ld hl, .pressStartMenu
+  ld a, h
+  ld [wVRAMCopyBuffer], a
+  ld a, l
+  ld [wVRAMCopyBuffer+1], a
+  ld a, VRAMCOPY_POINTER
+  ld [wVRAMCopyType], a
+
+  halt
 
   jr .skipFirstScreenLoading
 .maingameScreenLoading
 
   jr .skipFirstScreenLoading
-.skipFirstScreenLoading
+
+
+
+PRESS_START_SIZE equ 12
+.pressStartMenu:
+  db $13, $15, $8, $16, $16, $0, $0, $16, $17, $4, $15, $17
+.skipFirstScreenLoading:
