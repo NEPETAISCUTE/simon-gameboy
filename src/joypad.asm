713965d3049aa0ff1bd3d1c277bb2da0b46f6d15
nepecute <nepecute@penguin.lxd>
2021-06-18 11:09:03 +0200

    added a mask to filter out what i wrote myself in the joypad register

--- src/joypad.asm
+++ src/joypad.asm
@@ -1,5 +1,7 @@
 INCLUDE "src/lib/hardware.inc"
 
+JOYPADMASK EQU %00001111
+
 SECTION "Joypad", ROM0
 
 ReadJoypad::
@@ -13,6 +15,7 @@ ReadJoypad::
   ldh a, [rP1]
   ldh a, [rP1]
   cpl
+  and a, JOYPADMASK
   ld b, a
 
   ld a, P1F_GET_BTN
@@ -25,8 +28,12 @@ ReadJoypad::
   ldh a, [rP1]
   ldh a, [rP1]
   cpl
+  and a, JOYPADMASK
   swap a
 
   or a, b
   ld [Joypad1], a
+
+  ld a, P1F_GET_NONE
+  ld [rP1], a
   ret
