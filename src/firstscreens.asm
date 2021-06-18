bef972c5d4b2f937b3fd2355be40c8bb62b91578
nepecute <nepecute@penguin.lxd>
2021-06-18 13:58:15 +0200

    the render clears the "press start" message (and probably the future "game over" message) at the first frame of the main game gamestate

--- src/firstscreens.asm
+++ src/firstscreens.asm
@@ -1,18 +1,18 @@
   ld a, [isFirstFrame] ;checks if it's the first frame of that specific game state
-  or a, 0
-  jr z, .skipFirstScreenLoading
+  cp a, 0
+  jp z, .skipFirstScreenLoading
 
   ld a, [gameState]
-  or a, 0
+  cp a, 0
   jr z, .titleScreenLoading
 
   ld a, [gameState]
-  xor a, 1
+  cp a, 1
   jr z, .maingameScreenLoading
 
 .gameoverScreenLoading
 
-  jr .skipFirstScreenLoading
+  jp .skipFirstScreenLoading
 .titleScreenLoading
   xor a, a
   ld [isFirstFrame], a
@@ -24,10 +24,10 @@
 
   ld a, 13
   ld [wVRAMCopyLen], a
+  ld c, a
 
   xor a, a
   ld [wVRAMCopyType], a
-  ld c, 13
   ld hl, wVRAMCopyBuffer
 .zerofillBuffer
   ld [hli], a
@@ -68,7 +68,27 @@
 
   jr .skipFirstScreenLoading
 .maingameScreenLoading
+  xor a, a
+  ld [isFirstFrame], a
 
+  ld a, $9A
+  ld [wVRAMCopyDest], a
+  ld a, $04
+  ld [wVRAMCopyDest+1], a
+
+  ld a, 12
+  ld [wVRAMCopyLen], a
+  ld c, a
+
+  xor a, a
+  ld [wVRAMCopyType], a
+  ld hl, wVRAMCopyBuffer
+.zerofillBuffer2
+  ld [hli], a
+  dec c
+  jr nz, .zerofillBuffer2
+
+  halt
   jr .skipFirstScreenLoading
 
 
