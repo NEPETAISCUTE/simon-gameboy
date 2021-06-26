e6e3f702116b268c987b58be6739aa8603be1acc
nepecute <nepecute@penguin.lxd>
2021-06-26 13:31:49 +0200

    added an easy generation mode for level 1 to 4, it generates only 1 input at a time instead of generating combinations of multiple outputs

--- src/generateInputList.asm
+++ src/generateInputList.asm
@@ -7,11 +7,34 @@ generateInputList::
   ld c, a
 
   ld de, wInputList
-.generationLoop:
+  ld a, [wLevel]
+  cp a, 4
+  jr nc, .generationLoopHard
+.generationLoopEasy:
   push bc
   call rand
   pop bc
 
+  ld b, a
+  swap a
+  add a, b
+  adc a, 1
+  swap a
+  and %00000011
+
+  ld b, a
+  ld a, 1
+  call LoopShiftLeft
+
+  ld [de], a
+  inc de
+  dec c
+  jr nz, .generationLoopEasy
+  jr .end
+.generationLoopHard:
+  push bc
+  call rand
+  pop bc
   ;apparently this algorithm is the same as A%15, i still don't understand exactly how it works, but it's fine
   ld b, a
   swap a
@@ -22,11 +45,10 @@ generateInputList::
 
   inc a
 
-
   ld [de], a
   inc de
   dec c
-  jr nz, .generationLoop
-
+  jr nz, .generationLoopHard
+.end:
   xor a, a
   ld [wIsGeneratingNewInput], a
